ece364374173ec2163783b4d221b75ad2efdfc4c
louis00009 <lzb674877967@gmail.com>
2024-05-19 14:15:16 +1000

    email
    email

--- src/components/AuctionEmail.js
+++ src/components/AuctionEmail.js
@@ -4,7 +4,21 @@ const EmailButton = () => {
   const handleClick = () => {
     const recipient = "aaa@gmail.com";
     const subject = encodeURIComponent("固定的邮件主题");
-    const body = encodeURIComponent("这是邮件的正文部分");
+    const body = encodeURIComponent(`
+    <div>
+ZHANBO LIU
+<b>Business Analyst & Data Administrator</b>
+
+<img src="public\companylogo.png"><b>Australia</b>
+201, 12-14 Cato Street,
+Hawthorn East. Victoria 3123
+ 
+Tel: +61 3 9020 0820
+Email: zbl@autorola.com.au
+Web: www.autorola.com.au
+
+</div>
+    `);
 
     window.location.href = `mailto:${recipient}?subject=${subject}&body=${body}`;
   };
